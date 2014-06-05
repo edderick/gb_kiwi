c11f816e465b15d3cc53e005ffd8db83ad456cb1
Edward Seabrook <edderick@live.co.uk>
2014-06-05 01:02:41 +0100

    Changed NULL to NONE to prevent shadowing

--- Memory.cpp
+++ Memory.cpp
@@ -1,18 +1,21 @@
 #include "Memory.h"
+#include <iostream>
+#include <iomanip> 
+#include <string>
 
 char& Memory::operator[](unsigned int i) {
     if (i < 0x4000) {
         //ROM Bank 0
-        return NULL;
+        return cartridge[i];
     } else if (i < 0x8000) {
         //Switchable ROM Bank 
-        return NULL;
+        return NONE;
     } else if (i < 0xA000) {
         //Video RAM 
-        return NULL;
+        return NONE;
     } else if (i < 0xC000) {
         //Switchable RAM Bank 
-        return NULL; 
+        return NONE; 
     } else if (i < 0xE000) {
         //Internal RAM
         return main_mem[i - 0xC000];
@@ -21,16 +24,16 @@ char& Memory::operator[](unsigned int i) {
         return main_mem[i - 0xE000];
     } else if (i < 0xFEA0) {
         //Sprite Attrib Memory (OAM)
-        return NULL;
+        return NONE;
     } else if (i < 0xFF00) {
         //Empty but unusable I/O
-        return NULL;
+        return NONE;
     } else if (i < 0xFF4C) {
         //I/O Ports
-        return NULL;
+        return NONE;
     } else if (i < 0xFF80) {
         //Empty but unusable for I/O
-        return NULL;
+        return NONE;
     } else if (i < 0xFFFF) {
         //Internal RAM 
         return hi_mem[i - 0xFF80]; 
--- Memory.h
+++ Memory.h
@@ -3,7 +3,7 @@
 
 class Memory {
     char interrupt_enable_register; 
-    char NULL; // Placeholder...
+    char NONE; // Placeholder...
     char main_mem[0xE000 - 0xC000];
     char hi_mem[0xFFFF - 0xFF80];
 public: 
