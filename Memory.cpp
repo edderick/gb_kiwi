b37d759e8ce77105fc84230555d786e4d17019f0
Edward Seabrook <edderick@live.co.uk>
2014-06-06 01:34:47 +0100

    Improve readability of memory a little

--- Memory.cpp
+++ Memory.cpp
@@ -14,39 +14,51 @@ unsigned char& Memory::operator[](unsigned int i) {
     } else if (i < 0x4000) {
         //ROM Bank 0
         return cartridge[i];
-    } else if (i < 0x8000) {
+
+    } else if ((i >= 0x4000) && (i < 0x8000)) {
         //Switchable ROM Bank 
-        return NONE;
-    } else if (i < 0xA000) {
+        return cartridge[i];
+
+    } else if ((i >= 0x8000) && (i < 0xA000)) {
         //Video RAM 
-        return NONE;
-    } else if (i < 0xC000) {
+        return VRAM[i - 0x8000];
+
+    } else if ((i >= 0xA000) && (i < 0xC000)) {
         //Switchable RAM Bank 
         return NONE; 
-    } else if (i < 0xE000) {
+
+    } else if ((i >= 0xC000) && (i < 0xE000)) {
         //Internal RAM
         return main_mem[i - 0xC000];
-    } else if (i < 0xFE00) {
+
+    } else if ((i >= 0xE000) && (i < 0xFE00)) {
         //Echo of Internal RAM
         return main_mem[i - 0xE000];
-    } else if (i < 0xFEA0) {
+
+    } else if ((i >= 0xFE00) && (i < 0xFEA0)) {
         //Sprite Attrib Memory (OAM)
         return NONE;
-    } else if (i < 0xFF00) {
+
+    } else if ((i >= 0xFEA0) && (i < 0xFF00)) {
         //Empty but unusable I/O
         return NONE;
-    } else if (i < 0xFF4C) {
+
+    } else if ((i >= 0xFF00) && (i < 0xFF4C)) {
         //I/O Ports
         return IO_Ports[i - 0xFF00];
-    } else if (i < 0xFF80) {
+
+    } else if ((i >= 0xFF4C) && (i < 0xFF80)) {
         //Empty but unusable for I/O
         return NONE;
-    } else if (i < 0xFFFF) {
+
+    } else if ((i >= 0xFF80) && (i < 0xFFFF)) {
         //Internal RAM 
         return hi_mem[i - 0xFF80]; 
+
     } else if (i == 0xFFFF) {
         // Interrupt Enable Register
         return interrupt_enable_register;
+
     } else { 
         //Some  kind of error? 
         return NONE;
--- Memory.h
+++ Memory.h
@@ -9,7 +9,9 @@ class Memory {
     unsigned char NONE; // Placeholder...
     unsigned char main_mem[0xE000 - 0xC000];
     unsigned char hi_mem[0xFFFF - 0xFF80];
+    //TODO: Pull these out into actual classes
     unsigned char IO_Ports[0xFF4C - 0xFF00];
+    unsigned char VRAM[0xA000 - 0x8000];
     bool is_DMG();
 public: 
     Cartridge cartridge; 
