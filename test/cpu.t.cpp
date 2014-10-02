8729be175127e2c927e70f7f9d0e1935b79aced8
Edward Seabrook <edderick@live.co.uk>
2014-10-02 23:30:49 +0100

    Began adding unit tests

--- test/cpu.t.cpp
+++ test/cpu.t.cpp
@@ -1,5 +1,28 @@
 #include <gtest/gtest.h>
+#include "../cpu.h"
 
-TEST(foo, bar) {
+TEST(CPU, cpuCanBeCreated) {
+    CPU cpu; 
+    EXPECT_EQ(0, cpu.PC); 
+}
 
+TEST(CPU, 8_bitLoadsWordCorrectly) {
+    CPU cpu; 
+
+    unsigned char expected = 5;
+    cpu.LD(cpu.A, expected);
+
+    EXPECT_EQ(expected, cpu.A);
+}
+
+TEST(CPU, 16_bitLoadsWordCorrectly) {
+    CPU cpu; 
+    
+    unsigned char expectedA = 5;
+    unsigned char expectedB = 6;
+    
+    cpu.LD_16(cpu.A, cpu.B, expectedA, expectedB);
+
+    EXPECT_EQ(expectedA, cpu.A);
+    EXPECT_EQ(expectedB, cpu.B);
 }
